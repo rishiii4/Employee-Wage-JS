88d888a41e93bb75a8cf6d2e90d590a604302d44
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-03-06 22:16:06 +0530

    [Pranav] Add. GetWorkHours Function

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -4,12 +4,6 @@ const IS_PRESENT = 1;
 // Generate Random Number (0 or 1)
 let empCheck = Math.floor(Math.random() * 2); 
 
-// Check if Employee is Present or Absent
-if (empCheck === IS_PRESENT) {
-    console.log("Employee is Present");
-} else {
-    console.log("Employee is Absent");
-}
 
 // UC2: Calculate Daily Employee Wage based on Part-time or Full-time
 const NO_WORK = 0;
@@ -19,28 +13,27 @@ const WAGE_PER_HOUR = 20;
 
 let empHours = 0;
 
-if (empCheck === IS_PRESENT) {
-    console.log("Employee is Present");
-
-    // Work Type Check (0 = No Work, 1 = Part Time, 2 = Full Time)
+// UC3: Function to Get Work Hours
+function getWorkHours() {
     let workType = Math.floor(Math.random() * 3); 
-
     switch (workType) {
         case 0:
-            empHours = NO_WORK;
             console.log("Employee did No Work Today");
-            break;
+            return NO_WORK;
         case 1:
-            empHours = PART_TIME;
             console.log("Employee worked Part Time (4 Hours)");
-            break;
+            return PART_TIME;
         case 2:
-            empHours = FULL_TIME;
             console.log("Employee worked Full Time (8 Hours)");
-            break;
+            return FULL_TIME;
         default:
-            empHours = NO_WORK;
+            return NO_WORK;
     }
+}
+
+if (empCheck === IS_PRESENT) {
+    console.log("Employee is Present");
+    empHours = getWorkHours(); 
 } else {
     console.log("Employee is Absent");
 }
